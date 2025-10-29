9f65b02184f6ec676776116f1f516ab3949bb48b
joaoscotto <joao.scotto@gmail.com>
2025-10-29 01:56:07 -0300

    chore(config/routes): add health check

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
+
   resources :profiles, only: [ :index, :update, :create, :destroy ]
 
   root "profiles#index"
